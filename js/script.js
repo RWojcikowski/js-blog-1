705901b804970ce28c3d42a96ef14ae864d8e245
RWojcikowski <Radoslaw.chicqen@gmail.com>
2021-06-07 21:29:48 +0200

    poprawa zadania

--- js/script.js
+++ js/script.js
@@ -18,11 +18,12 @@ function titleClickHandler(event) {
   clickedElement.classList.add(".active");
   
   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll(".titles a.active");
 
-  for (let activeArticle of activeArticles) {
-    activeArticle.classList.remove(".post active");
-  }
+  const activeArticles = document.querySelectorAll(".post");
+
+  // for (let activeArticle of activeArticles) {
+  //   activeArticle.classList.remove("active");
+  // }
   /* get 'href' attribute from the clicked link */
   const articleId = clickedElement.href.split("#")[1];
   console.log(articleId);
@@ -45,3 +46,41 @@ const links = document.querySelectorAll(".titles a");
 for (let link of links) {
   link.addEventListener("click", titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+ 
+  /* remove contents of titleList */
+const listTitle = document.querySelector('.titles');
+console.log(listTitle);
+listTitle.innerHTML = '';
+  
+  /* for each article */
+  const articles = document.querySelector('.post');
+
+for (let post of posts) {
+  console.log(articles);
+}
+
+    /* get the article id */
+    
+    const linkHTML = '<li><a href="#' + articleId +  '"><span>' + articleTitle +  '</span></a></li>';
+    
+    console.log(linkHTML)
+    
+    /* find the title element */
+    
+    titleList.innerHTML = titleList.innerHTML + linkHTML;
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
